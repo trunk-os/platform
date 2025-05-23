432249f1fd8cfead3ed15081828807e642258ffe
Erik Hollensbe <git@hollensbe.org>
2025-05-23 16:35:20 -0700

    it's all working properly now
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- containers/include.mk
+++ containers/include.mk
@@ -13,8 +13,8 @@ all: .stamp.make
 	touch .stamp.make
 
 build-default:
-	podman build $(BUILD_ARGS) -t "$(TAG)" .
-	podman tag "$(TAG)" "$(LATEST_TAG)"
+	sudo podman build $(BUILD_ARGS) -t "$(TAG)" .
+	sudo podman tag "$(TAG)" "$(LATEST_TAG)"
 
 clean-default:
 	rm -f .stamp.make
@@ -22,10 +22,10 @@ clean-default:
 pre-down-default:
 
 down-default: pre-down
-	podman rm -f $(NAME)
+	sudo podman rm -f $(NAME)
 
 run-default: down
-	podman run -it -d --name $(NAME) $(TAG)
+	sudo podman run -it -d --name $(NAME) $(TAG)
 
 %: %-default
 	@true
