33189d487f0ada5d34ae50b2ec5dab3388b5e65d
Christopher Maujean <christopher@maujean.org>
2025-07-03 19:58:03 -0700

    Fix panel container to not need .env files
    YOU MUST SET THE API ENVIRONMENT VARIABLE before podman run
    
    Production:
    VITE_API_BASE_URL=http://trunk.local:5309

---
+++ containers/panel/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+# Create config.ts with environment variables
+echo "export const config = { API_BASE_URL: '${VITE_API_BASE_URL:-http://localhost:5309}', ENABLE_DEBUG_TOOLS: 'false' };" > /srv/src/config.ts
+
+# Start caddy
+exec caddy run --config /etc/caddy/Caddyfile
