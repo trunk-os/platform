2b8e200ac3299adc07cb6dab95a3855073daf368
Christopher Maujean <christopher@maujean.org>
2025-07-03 20:48:20 -0700

    fixing entrypoint, default value in include.mk

--- containers/panel/entrypoint.sh
+++ containers/panel/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Create config.ts with environment variables
-echo "export const config = { API_BASE_URL: '${VITE_API_BASE_URL:-http://localhost:5309}', ENABLE_DEBUG_TOOLS: 'false' };" > /srv/src/config.ts
+echo "export const config = { API_BASE_URL: '${VITE_API_BASE_URL:-http://trunk.local:5309}', ENABLE_DEBUG_TOOLS: 'false' };" > /srv/config.ts
 
 # Start caddy
 exec caddy run --config /etc/caddy/Caddyfile
